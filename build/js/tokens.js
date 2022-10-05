4af7d291b2c7251ac98fdcad99f24006df7e1d15
Luis Henrique Carreira <luishenriquecarreira@gmail.com>
2022-10-05 19:28:32 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Oct 2022 22:25:07 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8257e6"
+};
